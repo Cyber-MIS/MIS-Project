2610ffa8184843904a3f95fe9caccef5482af30e
muaadh99 <muaadhmnm.20@uom.lk>
2023-09-24 06:53:10 +0530

    Login Page UI improved

--- frontend/src/pages/authentication/login.js
+++ frontend/src/pages/authentication/login.js
@@ -1,97 +1,108 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { Button, Card } from 'antd';
-import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 import { login } from '../../api/authentication';
 
-export default function Login() {
+export default function UserLoginReg() {
   const loginSchema = Yup.object().shape({
     username: Yup.string().required(),
+    password: Yup.string().required(),
   });
 
-  // const navigate = useNavigate();
   const [loggingError, setLoginError] = useState(false);
-
   const navigate = useNavigate();
 
   const handleSubmit = (values, { setSubmitting }) => {
     setSubmitting(true);
-    const loginDeatails = {
+    const loginDetails = {
       username: values.username,
       password: values.password,
     };
 
-    login({ loginDeatails })
+    login({ loginDetails })
       .then((response) => {
-        console.log(response);
         setSubmitting(false);
         if (response.auth === 'success') {
           navigate('/dashboard');
         }
       })
       .catch((err) => {
-        console.log(err);
         setLoginError(true);
         setSubmitting(false);
       });
   };
 
   return (
-    <div>
-      <Card title='Login' className='login--card'>
-        <Formik
-          initialValues={{
-            username: '',
-            password: '',
-          }}
-          validationSchema={loginSchema}
-          onSubmit={handleSubmit}
-        >
-          {(props) => {
-            const errorInputStyle = {
-              borderColor: 'red',
-            };
-            return (
-              <Form className='user--login--form'>
-                <span>
-                  <Field
-                    type='text'
-                    name='Username'
-                    placeholder='Username'
-                    style={
-                      props.touched.name && props.errors.name
-                        ? errorInputStyle
-                        : null
-                    }
-                  />
-                </span>
-                <span>
-                  <Field type='Password' name='Password' placeholder='Password' />
-                </span>
-
-                <Button
-                  className='user--login--form--submit'
-                  type='primary'
-                  danger = {loggingError}
-                  onClick={props.handleSubmit}
-                  disabled={props.isSubmitting}
-                >
-                  Submit
-                </Button>
-                {Object.values(props.touched).includes(true) &&
-                  Object.values(props.errors).length !== 0 && (
-                    <div className='user--reg--form--errors'>
-                      <ErrorMessage name='username' component='div' />
-                      <ErrorMessage name='password' component='div' />
-                    </div>
-                  )}
-              </Form>
-            );
-          }}
-        </Formik>
-      </Card>
+    <div className="container mt-5 ">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header text-center ">Login</div>
+            <div className="card-body">
+              <Formik
+                initialValues={{
+                  username: '',
+                  password: '',
+                }}
+                validationSchema={loginSchema}
+                onSubmit={handleSubmit}
+              >
+                {(props) => {
+                  return (
+                    <Form>
+                      <div className="mb-3">
+                        <Field
+                          type="text"
+                          name="username"
+                          placeholder="Username"
+                          className={`form-control ${
+                            props.touched.username && props.errors.username
+                              ? 'is-invalid'
+                              : ''
+                          }`}
+                        />
+                        <ErrorMessage
+                          name="username"
+                          component="div"
+                          className="invalid-feedback"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <Field
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                          className="form-control"
+                        />
+                        <ErrorMessage
+                          name="password"
+                          component="div"
+                          className="invalid-feedback"
+                        />
+                      </div>
+                      <button
+                        type="submit"
+                        className={`btn btn-primary btn-block ${
+                          loggingError ? 'btn-danger' : ''
+                        }`}
+                        disabled={props.isSubmitting}
+                      >
+                        Submit
+                      </button>
+                    </Form>
+                  );
+                }}
+              </Formik>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
+
+
