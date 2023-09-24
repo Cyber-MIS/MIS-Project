ceb0e91688721f53f6b00b09f521fd57d02f3cba
muaadh99 <muaadhmnm.20@uom.lk>
2023-09-24 06:59:24 +0530

    Register Form Styled

--- frontend/src/pages/authentication/registerForm.js
+++ frontend/src/pages/authentication/registerForm.js
@@ -1,8 +1,8 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { Button } from 'antd';
-import { addUser } from '../../api/authentication';   //
 import * as Yup from 'yup';
-// Use this instead https://github.com/jannikbuschke/formik-antd
+
+import { addUser } from '../../api/authentication';
+
 export default function UserReg() {
   const userRegSchema = Yup.object().shape({
     name: Yup.string().required(),
@@ -21,61 +21,90 @@ export default function UserReg() {
     };
     addUser({ user }).then(() => setSubmitting(false));
   };
-  return (
-    <div>
-      <Formik
-        initialValues={{
-          Name: '',
-          Password: '',
-          Age: '',
-          Field: '',
-        }}
-        validationSchema={userRegSchema}
-        onSubmit={handleSubmit}
-      >
-        {(props) => {
-          const errorInputStyle = {
-            borderColor: 'red',
-          };
-          return (
-            <Form className='user--reg--form'>
-              <h1>Guest User Registration</h1>
-              <span>
-                <Field
-                  type='text'
-                  name='Name'
-                  placeholder='Name'
-                />
-              </span>
-              <span>
-                <Field type='text' name='Password' placeholder='Password' />
-              </span>
-              <span>
-                <Field type='text' name='Age' placeholder='Age' />
-              </span>
-              <span>
-                <Field type='text' name='Field' placeholder='Field' />
-              </span>
-           
-              <Button
-                className='user--reg--form--submit'
-                type='primary'
-                onClick={props.handleSubmit}
-                disabled={props.isSubmitting}
-              >
-                Submit
-              </Button>
 
-              <div className='user--reg--form--errors'>
-                <ErrorMessage name='name' component='div' />
-                <ErrorMessage name='password' component='div' />
-                <ErrorMessage name='age' component='div' />
-                <ErrorMessage name='field' component='div' />
-              </div>
-            </Form>
-          );
-        }}
-      </Formik>
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h1 className="text-center mb-4">Guest User Registration</h1>
+          <Formik
+            initialValues={{
+              name: '',
+              password: '',
+              age: '',
+              field: '',
+            }}
+            validationSchema={userRegSchema}
+            onSubmit={handleSubmit}
+          >
+            {(props) => {
+              return (
+                <Form>
+                  <div className="mb-3">
+                    <Field
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <Field
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <Field
+                      type="text"
+                      name="age"
+                      placeholder="Age"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="age"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <Field
+                      type="text"
+                      name="field"
+                      placeholder="Field"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="field"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-block"
+                    disabled={props.isSubmitting}
+                  >
+                    Submit
+                  </button>
+                </Form>
+              );
+            }}
+          </Formik>
+        </div>
+      </div>
     </div>
   );
 }
