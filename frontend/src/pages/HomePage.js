1b9503f154e4a13e902b3665ae5736ee00e387be
muaadh99 <muaadhmnm.20@uom.lk>
2023-10-05 16:33:53 +0530

    Header and Footer added to the homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'antd';
-
-import Logo from './Images/logo-no-background.png'
+import Header from '../Components/header';
+import Footer from '../Components/footer';
+import Sidebar from '../Components/sidebar';
 import './PageStyling/HomePage.css';
+import Navbar from '../Components/navbar';
 
 export default function HomePage() {
 
     const navigate = useNavigate();
 
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column', // Stack children vertically
+        justifyContent: 'center', // Center children vertically
+        alignItems: 'center',     // Center children horizontally
+        height: '70vh'           // Take full viewport height
+    }
+    
+    const logoStyle = {
+        marginBottom: '20px'      // Increase this value to increase the gap
+    }
+    
+    
+
     return (
         <div className="HomePage">
-            <div className='navbar'>
-                <div className='logo'>
-                    <img className='logo-image' src={Logo} alt='logo' />
-                </div>
+            <Navbar />
+            <div style={containerStyle}>
+
                 <div className='navlinks'>
                     <Button className='button' type="primary" onClick={() => navigate('/login')}>Login</Button>
                     <Button className='button' type="primary" onClick={() => navigate('/register')}>Register</Button>
+                    <Button className='button' type="primary" onClick={() => navigate('/donate')}>Donate</Button>
                 </div>
             </div>
+        <Footer />
         </div>
+
     );
 
 }
